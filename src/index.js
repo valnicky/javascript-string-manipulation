2fa70c27e0049b0d1b6e90dd6114458a17137e2b
Valeria <valery0007@gmail.com>
2020-04-13 19:09:18 +0200

    Symbol

--- src/index.js
+++ src/index.js
@@ -24,9 +24,9 @@ const transformLineEnding = (string, lineEnding) => {
 };
 
 const LineEndings = {
-  CR: "CR",
-  LF: "LF",
-  CRLF: "CRLF"
+  Symbol(CR): "CR",
+  Symbol(LF): "LF",
+  Symbol(CRLF): "CRLF"
 };
 
 const LineEndingReplacements = {
