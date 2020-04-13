d14f58e4cdb41b7bc985e4baed163c11ac18a0ce
Valeria <valery0007@gmail.com>
2020-04-13 18:27:46 +0200

    template literal

--- src/info.js
+++ src/info.js
@@ -1,13 +1,15 @@
 const usage = "Usage: crlf-convert [CR|LF|CRLF]";
 
 const help = `${usage}  
-   Converts text between newline (\\n), carriage return (\\r), and combined
-  (\\r\\n) line endings.
+
+
+   Converts text between newline (`\`\n), carriage return (`\`\r), and combined
+  (`\`\r`\`\n) line endings.
   Options:
   [CR|LF|CRLF]: Desired line ending
-      CR is a carriage return character \\r (rare)
-      LF is a line feed character \\n (common on Unix and macOS)
-      CRLF is a combined ending \\r\\n (common on Windows)
+      CR is a carriage return character `\`\r (rare)
+      LF is a line feed character `\`\n (common on Unix and macOS)
+      CRLF is a combined ending `\`\r`\`\n (common on Windows)
   Example:
   
       cat in.txt | crlf-convert LF > out.txt`;
