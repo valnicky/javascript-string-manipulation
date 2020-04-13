5d2f2b7c9f9272b3feede61685cb6d74d75acae2
Valeria <valery0007@gmail.com>
2020-04-13 22:00:32 +0200

    info js

--- src/info.js
+++ src/info.js
@@ -8,8 +8,8 @@ const help = `${usage}
   Options:
   [CR|LF|CRLF]: Desired line ending
       CR is a carriage return character String.raw \\r (rare)
-      LF is a line feed character String.raw `\\n` (common on Unix and macOS)
-      CRLF is a combined ending String.raw `\\r`\\n`(common on Windows)
+      LF is a line feed character String.raw \\n (common on Unix and macOS)
+      CRLF is a combined ending String.raw \\r\\n(common on Windows)
 Example:
 
 cat in.txt | crlf - convert LF > out.txt`;
