d2d433e58926c6fb69f052c9f65d42db5a35a617
Valeria <valery0007@gmail.com>
2020-04-13 21:57:50 +0200

    info js

--- src/info.js
+++ src/info.js
@@ -3,11 +3,11 @@ const usage = "Usage: crlf-convert [CR|LF|CRLF]";
 const help = `${usage}  
 
 
-   Converts text between newline String.raw `(\\n)`, carriage return String.raw `(\\r)`, and combined
-  String.raw `(\\r)``(\\n)` line endings.
+   Converts text between newline String.raw (\\n), carriage return String.raw (\\r), and combined
+  String.raw (\\r)(\\n) line endings.
   Options:
   [CR|LF|CRLF]: Desired line ending
-      CR is a carriage return character String.raw `\\r` (rare)
+      CR is a carriage return character String.raw \\r (rare)
       LF is a line feed character String.raw `\\n` (common on Unix and macOS)
       CRLF is a combined ending String.raw `\\r`\\n`(common on Windows)
 Example:
