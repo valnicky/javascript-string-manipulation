4ab7949d48983c8afe7a326dfc8efdc427eb73e6
Valeria <valery0007@gmail.com>
2020-04-13 21:55:10 +0200

    info.js literals

--- src/info.js
+++ src/info.js
@@ -4,15 +4,15 @@ const help = `${usage}
 
 
    Converts text between newline String.raw `(\\n)`, carriage return String.raw `(\\r)`, and combined
-  String.raw `(\\r``\\n)` line endings.
+  String.raw `(\\r)``(\\n)` line endings.
   Options:
   [CR|LF|CRLF]: Desired line ending
       CR is a carriage return character String.raw `\\r` (rare)
       LF is a line feed character String.raw `\\n` (common on Unix and macOS)
-      CRLF is a combined ending String.raw `\\r\\n` (common on Windows)
-  Example:
-  
-      cat in.txt | crlf-convert LF > out.txt`;
+      CRLF is a combined ending String.raw `\\r`\\n`(common on Windows)
+Example:
+
+cat in.txt | crlf - convert LF > out.txt`;
 
 module.exports = {
   help,
