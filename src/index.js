11c6497059ddc99d049ec36f4e284ba85dda09c8
Valeria <valery0007@gmail.com>
2020-04-15 11:12:49 +0200

    destructure LineEndingReplacements

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
 // Create a tagged template lf`...` that formats text using LF line endings.
-var lf = () => { };
+const lf = () => { };
 
 // Create a tagged template cr`...` that formats text using CR line endings.
-var cr = () => { };
+const cr = () => { };
 
 // Create a tagged template crlf`...` that formats text using CRLF line endings.
-var crlf = () => { };
+const crlf = () => { };
 
 const transformLineEnding = (string, lineEnding) => {
+  { replaceLF, replaceCR, replaceCRLF } = LineEndingReplacements;
   string = (string != null ? string.toString() : "");
 
   if (lineEnding === LineEndings.CR) {
-    string = LineEndingReplacements.replaceCRLF(string, "\r");
-    string = LineEndingReplacements.replaceLF(string, "\r");
+    string = replaceCRLF(string, "\r");
+    string = replaceLF(string, "\r");
   } else if (lineEnding === LineEndings.LF) {
-    string = LineEndingReplacements.replaceCRLF(string, "\n");
-    string = LineEndingReplacements.replaceCR(string, "\n");
+    string = replaceCRLF(string, "\n");
+    string = replaceCR(string, "\n");
   } else if (lineEnding === LineEndings.CRLF) {
-    string = LineEndingReplacements.replaceCR(string, "\r\n");
-    string = LineEndingReplacements.replaceLF(string, "\r\n");
+    string = replaceCR(string, "\r\n");
+    string = replaceLF(string, "\r\n");
   }
   return string;
 };
