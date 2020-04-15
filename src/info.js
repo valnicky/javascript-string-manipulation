e2db4f58934d7309add6a550798658bea161bf69
Valeria <valery0007@gmail.com>
2020-04-15 13:42:28 +0200

    fixing error

--- src/info.js
+++ src/info.js
@@ -3,7 +3,8 @@ const usage = "Usage: crlf-convert [CR|LF|CRLF]";
 const help = String.raw`${usage}  
 
 Converts text between newline (\n), carriage return (\r), and combined
-   (\r)(\n) line endings.
+(\r)(\n) line endings.
+   
    
 Options:
   
