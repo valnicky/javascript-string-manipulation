414689d0149e61f56ceeb9a90ddc1be06dc75108
Valeria <valery0007@gmail.com>
2020-04-15 14:39:49 +0200

    spaces info.js

--- src/info.js
+++ src/info.js
@@ -12,7 +12,7 @@ Options:
     CR is a carriage return character  \r (rare)
     LF is a line feed character  \n (common on Unix and macOS)
     CRLF is a combined ending  \r\n(common on Windows)
-      
+
 Example:
 
     cat in.txt | crlf - convert LF > out.txt`;
